b05f5d41c96090076474bb96478de839362dd405
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-08-06 18:48:57 +0400

    origin

--- src/BmiService.java
+++
@@ -1,7 +0,0 @@
-public class BmiService {
-    public int anthropometry (int weight, double height){
-        double index;
-        index = weight / (height * height);
-        return (int) index;
-    }
-}

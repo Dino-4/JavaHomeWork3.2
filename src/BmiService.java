f047716c5bcb3e6a0e5af3f47e964e707622c79d
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-08-07 14:09:05 +0400

    origin

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int anthropometry (int weight, double height){
+    public int anthropometry(int weight, double height) {
         double index;
         index = weight / (height * height);
         return (int) index;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    BmiService service = new BmiService();
-        int bmi = service.anthropometry(98,1.87);
+        BmiService service = new BmiService();
+        int bmi = service.anthropometry(98, 1.87);
         System.out.println(bmi);
 
 
